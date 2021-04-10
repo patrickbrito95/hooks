890286770584abc3219088644ec12c743307d343
Patrick Brito <patrickbrito95@gmail.com>
2021-04-10 14:25:54 -0300

    some 15 iterations done

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./Accordion";
+import Dropdown from "./Dropdown";
 import Search from "./Search";
 
 const items = [
@@ -17,13 +18,38 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: "The Color is Red",
+    value: "red",
+  },
+  {
+    label: "The Color is Green",
+    value: "green",
+  },
+  {
+    label: "The Color is Blue",
+    value: "blue",
+  },
+  {
+    label: "The Color is Orange",
+    value: "orange",
+  },
+];
+
 // Estamos chamando o Array de objetos 'items' dentro do componente 'Accordion' como props.
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div className="ui container">
       <h1>Widgets</h1>
       <Accordion items={items} />
       <Search />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  console.log(results);
+
+  // Só podemos chamar uma função assíncrona dentro de um useEffect apenas se colocarmos outra função dentro dela.
+  useEffect(() => {
+    const searchWiki = async () => {
+      const { data } = await axios.get("http://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+
+    if (term && !results.length) {
+      searchWiki();
+    } else {
+      // Aqui foi usado o setTimeout para limitar o número de requisições na API, a cada 0,5 segundo fará uma nova. Evitanto requisitar a cada dígito novo
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          searchWiki();
+        }
+      }, 500);
+      // Fiz essa função 'clearInterval' para poder limpar o setTimeout anterior, quando é utilizado pelo return dentro do useEffect
+      return () => {
+        clearInterval(timeoutId);
+      };
+    }
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {/* DANGEROUS SET INNER HTML SERVE PARA RENDERIZARMOS STRINGS PRONTAS VINDAS DA API COM FORMATO HTML */}
+          {/* DEVEMOS TER CERTEZA SE O HTML FORNECIDO PELO SERVIDOR SEJA 100% CONFIÁVEL, POIS ISSO PODE TORNAR NOSSO CÓDIGO VULNERÁVEL */}
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
@@ -15,6 +71,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };

1da32172d1de84cf44f4b3d1b311ab9207307795
Patrick Brito <patrickbrito95@gmail.com>
2021-04-14 18:27:47 -0300

    more 5

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import Accordion from "./Accordion";
 import Dropdown from "./Dropdown";
 import Search from "./Search";
-import Translate from './Translate'
+import Route from "./Route";
+import Header from "./Header";
 
 const items = [
   {
@@ -42,20 +43,38 @@ const options = [
 const App = () => {
   const [selected, setSelected] = useState(options[0]);
   const [showDropdown, setShowDropdown] = useState(true);
+
   return (
     <div className="ui container">
       <h1>Widgets</h1>
-      <Accordion items={items} />
-      <Search />
-      <button class="ui button" onClick={() => setShowDropdown(!showDropdown)}>Toggle Button</button>
-      { showDropdown ? <Dropdown
-        label={`Escolha uma cor: `}
-        selected={selected}
-        onSelectedChange={setSelected}
-        options={options}
-      /> : null
-      }
-      <Translate />
+
+      <Header />
+
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+
+      <Route path="/list">
+        <Search />
+      </Route>
+
+      <Route path="/dropdown">
+        <hr />
+        <button
+          class="ui button"
+          onClick={() => setShowDropdown(!showDropdown)}
+        >
+          Toggle Button
+        </button>
+        {showDropdown ? (
+          <Dropdown
+            label={`Escolha uma cor: `}
+            selected={selected}
+            onSelectedChange={setSelected}
+            options={options}
+          />
+        ) : null}
+      </Route>
     </div>
   );
 };
